f86ba67c5a38ea84909562df5eb54e91190ba343
Kevin Collins <kevin.collins@bydeluxe.com>
2017-08-23 14:04:51 -0700

    Initial version

---
+++ GitSharp/GitSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using LibGit2Sharp;
+
+namespace GitSharp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var repoPath = "";
+            using (var repo = new Repository(repoPath))
+            {
+                Console.WriteLine("Hello git");
+            }
+        }
+    }
+}
